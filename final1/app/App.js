bf28d30818b1d09e4da54a49fc9ff9df5365031b
David Joshua <davidjosh_87@yahoo.com>
2018-07-20 10:28:57 +0700

    masih ngaco

---
+++ final1/app/App.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('TicTacToe', () => App);
+
+
